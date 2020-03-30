8d39200539edfaae8b1302d1c6780a5cafc403c5
Andrea Santurbano <santand@gmail.com>
2020-03-30 17:31:46 +0200

    added config test

--- src/main/scala/org/neo4j/spark/Neo4jConfig.scala
+++ src/main/scala/org/neo4j/spark/Neo4jConfig.scala
@@ -7,9 +7,9 @@ import org.neo4j.driver.v1.{Driver, AuthTokens, Config, GraphDatabase}
  * @author mh
  * @since 02.03.16
  */
-case class Neo4jConfig(val url: String, val user: String = "neo4j", val password: Option[String] = None, encryptionStatus:Boolean) {
+case class Neo4jConfig(val url: String, val user: String = "neo4j", val password: Option[String] = None, encryption:Boolean) {
 
-  def boltConfig(): Config = if (encryptionStatus) Config.build().withEncryption().toConfig else Config.build().withoutEncryption().toConfig
+  def boltConfig(): Config = if (encryption) Config.build().withEncryption().toConfig else Config.build().withoutEncryption().toConfig
 
   def driver(config: Neo4jConfig) : Driver = config.password match {
     case Some(pwd) => GraphDatabase.driver(config.url, AuthTokens.basic(config.user, pwd), boltConfig())
