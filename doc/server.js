a9cfe78cb663ba35494ff711203a40b53a3b0f69
Guillaume Grossetie <ggrossetie@gmail.com>
2021-05-08 11:43:56 +0200

    Replace hyperlink by linkcheck (fasteeeer)

--- doc/server.js
+++ doc/server.js
@@ -1,10 +1,31 @@
+'use strict'
+
 const express = require('express')
+const portfinder = require('portfinder')
+const process = require('process')
 
-const app = express()
-app.use(express.static('./build/site'))
+process.on('message', (msg) => {
+  if (msg.event === 'exit') {
+    process.send({ event: 'exiting' })
+    process.exit(0)
+  }
+})
 
-app.use('/static/assets', express.static('./build/site/developer/_'))
-
-app.get('/', (req, res) => res.redirect('/developer/spark'))
-
-app.listen(8000, () => console.log('📘 http://localhost:8000'))
+;(async () => {
+  portfinder.basePort = 8000
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    }
+    const app = express()
+    app.use(express.static('./build/site'))
+    app.use('/static/assets', express.static('./build/site/developer/_'))
+    app.get('/', (req, res) => res.redirect('/developer/spark'))
+    app.listen(port, () => {
+      console.log(`📘 http://localhost:${port}`)
+      if (typeof process.send === 'function') {
+        process.send({ event: 'started', port: port });
+      }
+    })
+  })
+})()
--- doc/tasks/lint-links.js
+++ doc/tasks/lint-links.js
@@ -1,58 +1,75 @@
-const path = require('path')
-const hyperlink = require('hyperlink')
-const TapRender = require('@munter/tap-render');
+'use strict';
 
-const root = path.join(__dirname, '..')
+const ospath = require('path')
+const fs = require('fs')
+const childProcess = require('child_process')
+
+const express = require('express')
+const Download = require('bestikk-download')
+const download = new Download({})
+
+const root = ospath.join(__dirname, '..')
+const linkcheckVersion = 'v2.0.15%2B1'
+const linkcheckBinaryPath = ospath.join(root, 'bin', 'linkcheck')
 
 ;(async () => {
-  const tapRender = new TapRender()
-  tapRender.pipe(process.stdout)
-  try {
-    const skipPatterns = [
-      // initial redirect
-      'load index.html',
-      'load docs/index.html',
-      // google fonts
-      'load https://fonts.googleapis.com/',
-      // static resources
-      'load static/assets',
-      // externals links
-      // /
-      'load try-neo4j',
-      // /developer
-      'load developer',
-      // /labs
-      'load labs',
-      // /docs
-      'load docs',
-      // rate limit on twitter.com (will return 400 code if quota exceeded)
-      'external-check https://twitter.com/neo4j',
-      // workaround: not sure why the following links are not resolved properly by hyperlink :/
-      'load build/site/developer/spark/quickstart/reading',
-      'load build/site/developer/spark/quickstart/writing',
-      // aura kbase returns 403s for hyperlink
-      'external-check https://aura.support.neo4j.com',
-      // graphgist
-      'load graphgist',
-    ]
-    const skipFilter = (report) => {
-      return Object.values(report).some((value) => {
-          return skipPatterns.some((pattern) => String(value).includes(pattern))
-        }
-      )
-    };
-    await hyperlink({
-        root,
-        inputUrls: [`build/site/developer/spark/index.html`],
-        skipFilter: skipFilter,
-        recursive: true,
-      },
-      tapRender
-    )
-  } catch (err) {
-    console.log(err.stack);
-    process.exit(1);
+  // download linkcheck binary
+  if (fs.existsSync(linkcheckBinaryPath)) {
+    console.log(`${ospath.relative(root, linkcheckBinaryPath)} already exists, skipping download`)
+  } else {
+    const platform = process.platform
+    if (platform === 'darwin' || platform === 'linux') {
+      let os
+      if (platform === 'darwin') {
+        os = 'mac'
+      } else {
+        os = platform
+      }
+      await download.getContentFromURL(`https://github.com/filiph/linkcheck/releases/download/${linkcheckVersion}/linkcheck-${os}-x64.exe`, linkcheckBinaryPath)
+    } else {
+      throw new Error(`The platform ${platform} is not supported!`)
+    }
   }
-  const results = tapRender.close();
-  process.exit(results.fail ? 1 : 0);
+
+  // chmod +x
+  fs.chmodSync(linkcheckBinaryPath, 0o775)
+
+  // use "fork" to spawn a new Node.js process otherwise it creates a deadlock
+  const serverProcess = childProcess.fork(ospath.join(root, 'server.js'))
+  serverProcess.on('message', async (msg) => {
+    if (msg.event === 'started') {
+      // server has started!
+      try {
+        console.log(`${ospath.relative(root, linkcheckBinaryPath)} --skip-file ignore-links.txt :${msg.port}`)
+        const result = childProcess.execSync(`${linkcheckBinaryPath} --skip-file ignore-links.txt :${msg.port}`, { cwd: root, stdio: 'inherit' })
+        // stop server and exit gracefully
+        await new Promise((resolve, reject) => {
+          serverProcess.on('message', (msg) => {
+            if (msg.event === 'exiting') {
+              resolve()
+            }
+          })
+          serverProcess.send({ event: 'exit' })
+        })
+        process.exit(0)
+      } catch (error) {
+        if (error.stdout) {
+          console.log(error.stdout.toString('utf8'))
+        }
+        if (error.stderr) {
+          console.error(error.stderr.toString('utf8'))
+        }
+        await new Promise((resolve, reject) => {
+          serverProcess.on('message', (msg) => {
+            if (msg.event === 'exiting') {
+              resolve()
+            }
+          })
+          serverProcess.send({ event: 'exit' })
+        })
+        process.exit(1)
+      }
+    }
+  })
 })()
+
