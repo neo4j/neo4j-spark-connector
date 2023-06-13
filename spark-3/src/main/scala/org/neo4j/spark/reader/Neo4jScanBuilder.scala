240e4ba4918f001d3e429f8bd88c7cd509324418
Andrea Santurbano <andrea.santurbano@larus-ba.it>
2023-06-13 15:37:25 +0200

    Implement pushdown limit support
    Fixes #519
    
    Signed-off-by: Florent Biville <florent.biville@neo4j.com>

--- spark-3/src/main/scala/org/neo4j/spark/reader/Neo4jScanBuilder.scala
+++ spark-3/src/main/scala/org/neo4j/spark/reader/Neo4jScanBuilder.scala
@@ -1,9 +1,10 @@
 package org.neo4j.spark.reader
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.connector.expressions.Expression
 import org.apache.spark.sql.connector.expressions.aggregate.{AggregateFunc, Aggregation}
 import org.apache.spark.sql.connector.expressions.filter.Predicate
-import org.apache.spark.sql.connector.read.{Scan, SupportsPushDownAggregates, SupportsPushDownRequiredColumns, SupportsPushDownV2Filters}
+import org.apache.spark.sql.connector.read.{Scan, SupportsPushDownAggregates, SupportsPushDownLimit, SupportsPushDownRequiredColumns, SupportsPushDownV2Filters}
 import org.apache.spark.sql.types.{LongType, StructField, StructType}
 import org.neo4j.spark.util.Neo4jImplicits.{AggregationImplicit, CypherImplicits, PredicateImplicit}
 import org.neo4j.spark.util.{Neo4jOptions, QueryType}
@@ -11,7 +12,9 @@ import org.neo4j.spark.util.{Neo4jOptions, QueryType}
 class Neo4jScanBuilder(neo4jOptions: Neo4jOptions, jobId: String, schema: StructType)
   extends SupportsPushDownV2Filters
   with SupportsPushDownAggregates
-  with SupportsPushDownRequiredColumns {
+  with SupportsPushDownRequiredColumns
+  with SupportsPushDownLimit
+  with Logging {
 
   private var predicates: Array[Predicate] = Array.empty
 
@@ -19,9 +22,10 @@ class Neo4jScanBuilder(neo4jOptions: Neo4jOptions, jobId: String, schema: Struct
   private var requiredColumns: StructType = new StructType()
 
   private var aggregateColumns: Array[AggregateFunc] = Array.empty[AggregateFunc]
+  private var limit: Option[Int] = None
 
   override def build(): Scan = {
-    new Neo4jScan(neo4jOptions, jobId, requiredSchema, predicates.flatMap(_.toFilter), requiredColumns, aggregateColumns)
+    new Neo4jScan(neo4jOptions, jobId, requiredSchema, predicates.flatMap(_.toFilter), requiredColumns, aggregateColumns, limit)
   }
 
   override def pushPredicates(predicatesArray: Array[Predicate]): Array[Predicate] = {
@@ -77,6 +81,24 @@ class Neo4jScanBuilder(neo4jOptions: Neo4jOptions, jobId: String, schema: Struct
       requiredColumns = requiredColumns.add(StructField(af.toString, dt))
     })
     requiredSchema = requiredColumns
+    true
+  }
+
+  override def pushLimit(pushedLimit: Int): Boolean = {
+    if (!neo4jOptions.pushdownLimitEnabled) {
+      return false
+    }
+    if (neo4jOptions.partitions > 1) {
+      // TODO: improve wording
+      logWarning("disabling pushed down limit support since it conflicts with partitioning." +
+        "set the partition count to 1 or disable the pushdown limit support to remove this warning")
+      return false
+    }
+    if (pushedLimit <= 0) {
+      logWarning(s"ignoring negative pushed down limit $pushedLimit")
+      return false
+    }
+    limit = Some(pushedLimit)
     true
   }
 }
