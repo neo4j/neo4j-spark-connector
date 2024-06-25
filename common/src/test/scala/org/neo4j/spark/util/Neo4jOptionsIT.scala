7e95317b1512aa74b2f6e11aaf87f0d3b9cb811b
Ali Ince <ali.ince@neo4j.com>
2024-06-25 12:01:39 +0100

    Ignore test until driver behaviour changes

--- common/src/test/scala/org/neo4j/spark/util/Neo4jOptionsIT.scala
+++ common/src/test/scala/org/neo4j/spark/util/Neo4jOptionsIT.scala
@@ -1,7 +1,7 @@
 package org.neo4j.spark.util
 
 import org.junit.Assert.{assertEquals, assertNotNull}
-import org.junit.Test
+import org.junit.{Ignore, Test}
 import org.neo4j.spark.SparkConnectorScalaSuiteIT
 import org.neo4j.spark.SparkConnectorScalaSuiteIT.server
 
@@ -25,6 +25,7 @@ class Neo4jOptionsIT extends SparkConnectorScalaSuiteIT {
   }
 
   @Test
+  @Ignore("This requires a fix on driver, ignoring until it is implemented")
   def shouldConstructDriverWithResolver(): Unit = {
     val options: java.util.Map[String, String] = new java.util.HashMap[String, String]()
     options.put(Neo4jOptions.URL, s"neo4j://localhost.localdomain:8888, bolt://localhost.localdomain:9999, ${server.getBoltUrl}")
