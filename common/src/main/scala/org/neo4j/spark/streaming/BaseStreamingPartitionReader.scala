82cd91fef65a71779ed9b6671545843d48e415c2
Ali Ince <ali.ince@neo4j.com>
2024-09-12 23:39:25 +0100

    feat: rewrite query to apply from and to filters for stream queries

--- common/src/main/scala/org/neo4j/spark/streaming/BaseStreamingPartitionReader.scala
+++ common/src/main/scala/org/neo4j/spark/streaming/BaseStreamingPartitionReader.scala
@@ -21,12 +21,14 @@ import org.apache.spark.sql.sources.Filter
 import org.apache.spark.sql.sources.GreaterThan
 import org.apache.spark.sql.sources.LessThanOrEqual
 import org.apache.spark.sql.types.StructType
+import org.neo4j.cypherdsl.core.Cypher
 import org.neo4j.spark.reader.BasePartitionReader
 import org.neo4j.spark.service.Neo4jQueryStrategy
 import org.neo4j.spark.service.PartitionPagination
 import org.neo4j.spark.util.Neo4jImplicits._
 import org.neo4j.spark.util.Neo4jOptions
 import org.neo4j.spark.util.Neo4jUtil
+import org.neo4j.spark.util.QueryType._
 import org.neo4j.spark.util.StreamingFrom
 
 import java.util
@@ -64,7 +66,7 @@ class BaseStreamingPartitionReader(
   logInfo(s"Creating Streaming Partition reader $name")
 
   private lazy val values = {
-    val map = new util.HashMap[String, Any](super.getQueryParameters)
+    val map = new util.HashMap[String, Any](super.queryParameters)
     val start: Long = streamStart
       .flatMap(f => f.getValue)
       .getOrElse(StreamingFrom.ALL.value())
@@ -73,7 +75,7 @@ class BaseStreamingPartitionReader(
       .flatMap(f => f.getValue)
       .get // TODO: test this with emptied-after-checkpoint database (max(timestamp) would return NULL for the end)
       .asInstanceOf[Long]
-    map.put(Neo4jQueryStrategy.VARIABLE_STREAM, Map("offset" -> start, "end" -> end).asJava)
+    map.put(Neo4jQueryStrategy.VARIABLE_STREAM, Map("offset" -> start, "from" -> start, "to" -> end).asJava)
     map
   }
 
@@ -82,6 +84,30 @@ class BaseStreamingPartitionReader(
     super.close()
   }
 
-  override protected def getQueryParameters: util.Map[String, Any] = values
+  override protected def query: String = {
+    options.query.queryType match {
+      case QUERY =>
+        val property = Cypher.name(streamingPropertyName)
+        val stream = Cypher.parameter("stream")
+
+        // rewrite query for adding $stream.from and $stream.to filters
+        Cypher.callRawCypher(super.query)
+          .`with`(Cypher.asterisk())
+          .where(
+            property.gt(stream.property("from")).and(property.lte(stream.property("to")))
+          )
+          .returning(Cypher.asterisk())
+          .build()
+          .getCypher
+      // we don't need to rewrite the queries for LABELS and RELATIONSHIPS because spark filters already cover our
+      // criteria which are added to the query text in Neo4jQueryService
+      case LABELS       => super.query
+      case RELATIONSHIP => super.query
+      case GDS =>
+        throw new UnsupportedOperationException("GDS strategy is not supported in structured streaming use cases.")
+    }
+  }
+
+  override protected def queryParameters: util.Map[String, Any] = values
 
 }
