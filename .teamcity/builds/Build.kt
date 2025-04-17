67422a02d145cc1e6222926234b8f0afc851f694
Ali Ince <ali.ince@neo4j.com>
2025-04-17 19:05:19 +0100

    ci: make integration tests to wait on unit tests

--- .teamcity/builds/Build.kt
+++ .teamcity/builds/Build.kt
@@ -57,19 +57,19 @@ class Build(
                       ),
                   )
 
-                  parallel {
-                    neo4jVersions.forEach { neo4jVersion ->
-                      dependentBuildType(
-                          Maven(
-                              "${name}-unit-tests-${java.version}-${scala.version}-${neo4jVersion.version}",
-                              "unit tests (${java.version}, ${scala.version}, ${neo4jVersion.version})",
-                              "test",
-                              java,
-                              scala,
-                              neo4jVersion,
-                          ),
-                      )
+                  neo4jVersions.forEach { neo4jVersion ->
+                    dependentBuildType(
+                        Maven(
+                            "${name}-unit-tests-${java.version}-${scala.version}-${neo4jVersion.version}",
+                            "unit tests (${java.version}, ${scala.version}, ${neo4jVersion.version})",
+                            "test",
+                            java,
+                            scala,
+                            neo4jVersion,
+                        ),
+                    )
 
+                    parallel {
                       dependentBuildType(
                           JavaIntegrationTests(
                               "${name}-integration-tests-java-${java.version}-${scala.version}-${neo4jVersion.version}",
