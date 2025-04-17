bebc07c0a9d72894cb355116a03e769a0a1df1a8
Ali Ince <ali.ince@neo4j.com>
2025-04-17 13:33:36 +0100

    build: suppress output

--- maven-release.sh
+++ maven-release.sh
@@ -14,13 +14,13 @@ exit_script() {
   mv -f test-support/pom.xml.bak test-support/pom.xml
   mv -f spark-3/pom.xml.bak spark-3/pom.xml
   trap - SIGINT SIGTERM # clear the trap
-  kill -- -$$ # Sends SIGTERM to child/sub processes
+  kill -- -$$ || true # Sends SIGTERM to child/sub processes
 }
 
 mvn_evaluate() {
   local expression
   expression="${1}"
-  ./mvnw help:evaluate -Dexpression="${expression}" --quiet -DforceStdout
+  ./mvnw -B help:evaluate -Dexpression="${expression}" --quiet -DforceStdout
 }
 
 trap exit_script SIGINT SIGTERM
@@ -50,7 +50,7 @@ cp common/pom.xml common/pom.xml.bak
 cp test-support/pom.xml test-support/pom.xml.bak
 cp spark-3/pom.xml spark-3/pom.xml.bak
 
-./mvnw versions:set -DnewVersion=${PROJECT_VERSION}_for_spark_${SPARK_VERSION} -DgenerateBackupPoms=false
+./mvnw -B versions:set -DnewVersion=${PROJECT_VERSION}_for_spark_${SPARK_VERSION} -DgenerateBackupPoms=false
 
 # replace pom files with target scala version
 sed_i "s/<artifactId>neo4j-connector-apache-spark_parent<\/artifactId>/<artifactId>neo4j-connector-apache-spark_${SCALA_VERSION}_parent<\/artifactId>/" pom.xml
@@ -68,7 +68,7 @@ sed_i "s/<artifactId>neo4j-connector-apache-spark_test-support<\/artifactId>/<ar
 sed_i "s/<spark-packages.version\/>/<spark-packages.version>${SPARK_PACKAGES_VERSION}<\/spark-packages.version>/" "spark-3/pom.xml"
 
 # build
-./mvnw clean "${GOAL}" -Dscala-"${SCALA_VERSION}" -DskipTests ${ALT_DEPLOYMENT_REPOSITORY}
+./mvnw -B clean "${GOAL}" -Dscala-"${SCALA_VERSION}" -DskipTests ${ALT_DEPLOYMENT_REPOSITORY}
 
 if [ ! ${CI:-false} = true ]; then
   exit_script
