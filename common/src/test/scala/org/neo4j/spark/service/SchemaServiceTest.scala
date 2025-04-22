6277f5b9fbc58c3dda05465d94d4e08d01b76bcd
Florent Biville <florent.biville@neo4j.com>
2025-04-22 14:57:33 +0200

    fix: replace some Scala 2.13+ constructs

--- common/src/test/scala/org/neo4j/spark/service/SchemaServiceTest.scala
+++ common/src/test/scala/org/neo4j/spark/service/SchemaServiceTest.scala
@@ -18,26 +18,24 @@ package org.neo4j.spark.service
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.mockito.Mockito.RETURNS_DEEP_STUBS
-import org.mockito.Mockito.mock
-import org.neo4j.caniuse.Neo4j
-import org.neo4j.caniuse.Neo4jDeploymentType
-import org.neo4j.caniuse.Neo4jEdition
-import org.neo4j.caniuse.Neo4jVersion
+import org.mockito.Mockito.{RETURNS_DEEP_STUBS, mock}
+import org.neo4j.caniuse.{Neo4j, Neo4jDeploymentType, Neo4jEdition, Neo4jVersion}
 import org.neo4j.spark.config.TopN
-import org.neo4j.spark.util.DriverCache
-import org.neo4j.spark.util.Neo4jOptions
+import org.neo4j.spark.util.{DriverCache, Neo4jOptions}
 
-import scala.jdk.javaapi.CollectionConverters
+import scala.annotation.nowarn
+import scala.collection.JavaConverters
+
 
 class SchemaServiceTest {
 
   @Test
   def does_not_overflow_when_partition_size_is_over_max_value_of_32bit_integers(): Unit = {
+    // note: _ cannot be used to separate digit groups, as this requires Scala 2.13+
     val opts = options(
       "url" -> "bolt://example.com",
       "partitions" -> 2.toString,
-      "query.count" -> (2L * 2_147_483_648L).toString, // 2 * (Integer.MAX_VALUE + 1)
+      "query.count" -> (2L * 2147483648L).toString, // 2 * (Integer.MAX_VALUE + 1)
       "query" -> "MERGE (:Node)"
     )
     val schemaService = new SchemaService(neo4j(), opts, mock(classOf[DriverCache], RETURNS_DEEP_STUBS))
@@ -45,14 +43,15 @@ class SchemaServiceTest {
     val pages = schemaService.skipLimitFromPartition(Some(TopN(1024)))
 
     assertEquals(List(0, 1), pages.map(_.partitionNumber).toList)
-    assertEquals(List(0, 2_147_483_648L), pages.map(_.skip).toList)
-    assertEquals(List(2_147_483_648L, 2_147_483_648L), pages.map(_.topN.limit).toList)
+    assertEquals(List(0, 2147483648L), pages.map(_.skip).toList)
+    assertEquals(List(2147483648L, 2147483648L), pages.map(_.topN.limit).toList)
     assertEquals(List(0, 0), pages.map(_.topN.orders.size).toList)
   }
 
+  @nowarn // deprecated only in Scala 2.13, but 2.12 is our baseline
   private def options(kv: (String, String)*): Neo4jOptions = {
     new Neo4jOptions(
-      CollectionConverters.asJava(kv.toMap)
+      JavaConverters.mapAsJavaMap(kv.toMap)
     )
   }
 
