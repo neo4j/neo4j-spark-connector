4a75c7e887561aa2a87a6fa0252b9ceb85eeeab7
Erik Nord <erik.nord@neo4j.com>
2025-12-18 15:57:32 +0100

    chore: formatting

--- common/src/main/scala/org/neo4j/spark/converter/DataConverter.scala
+++ common/src/main/scala/org/neo4j/spark/converter/DataConverter.scala
@@ -76,7 +76,7 @@ class SparkToNeo4jDataConverter extends DataConverter[Value] {
 
   override def convert(value: Any, dataType: DataType): Value = {
     value match {
-      case date: java.sql.Date           => convert(date.toLocalDate, dataType)
+      case date: java.sql.Date => convert(date.toLocalDate, dataType)
       case timestamp: java.sql.Timestamp if dataType == DataTypes.TimestampType =>
         convert(timestamp.toInstant.atZone(ZoneOffset.UTC), dataType)
       case timestamp: java.sql.Timestamp if dataType == DataTypes.TimestampNTZType =>
