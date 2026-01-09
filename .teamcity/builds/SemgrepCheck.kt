70477b45108fd3394021d524f9340d84b899e70c
Ali Ince <ali-ince@users.noreply.github.com>
2026-01-09 10:01:28 +0000

    ci: add build failure condition for missing pr (#837)

--- .teamcity/builds/SemgrepCheck.kt
+++ .teamcity/builds/SemgrepCheck.kt
@@ -2,31 +2,27 @@ package builds
 
 import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
 
-class SemgrepCheck(
-    id: String,
-    name: String,
-    scalaVersion: ScalaVersion
-): Maven(
-    id,
-    name,
-    "dependency:tree",
-    JavaVersion.V_17,
-    scalaVersion,
-    Neo4jVersion.V_NONE,
-    "-DoutputFile=maven_dep_tree.txt"
-) {
+class SemgrepCheck(id: String, name: String, scalaVersion: ScalaVersion) :
+    Maven(
+        id,
+        name,
+        "dependency:tree",
+        JavaVersion.V_17,
+        scalaVersion,
+        Neo4jVersion.V_NONE,
+        "-DoutputFile=maven_dep_tree.txt") {
 
   init {
 
     params.password("env.SEMGREP_APP_TOKEN", "%semgrep-app-token%")
 
-    steps.step(ScriptBuildStep {
-      scriptContent="semgrep ci --no-git-ignore"
-      dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-      dockerImage = SEMGREP_DOCKER_IMAGE
-      dockerRunParameters =
-          "--volume /var/run/docker.sock:/var/run/docker.sock --volume %teamcity.build.checkoutDir%/signingkeysandbox:/root/.gnupg"
-    })
+    steps.step(
+        ScriptBuildStep {
+          scriptContent = "semgrep ci --no-git-ignore"
+          dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+          dockerImage = SEMGREP_DOCKER_IMAGE
+          dockerRunParameters =
+              "--volume /var/run/docker.sock:/var/run/docker.sock --volume %teamcity.build.checkoutDir%/signingkeysandbox:/root/.gnupg"
+        })
   }
-
 }
