9c2a57852ff63ac1ca41c850dc4b3de3bfe06687
Emre Hızal <emrehizal_772@hotmail.com>
2026-05-27 15:27:13 +0200

    Update src/test/scala/org/neo4j/spark/service/Neo4jQueryServiceTest.scala
    Co-authored-by: Florent Biville <445792+fbiville@users.noreply.github.com>

--- src/test/scala/org/neo4j/spark/service/Neo4jQueryServiceTest.scala
+++ src/test/scala/org/neo4j/spark/service/Neo4jQueryServiceTest.scala
@@ -1199,7 +1199,7 @@ class Neo4jQueryServiceTest {
   }
 
   @Test
-  def testScriptResult(): Unit = {
+  def testScriptResultInclusionInCustomQuery(): Unit = {
     val options: java.util.Map[String, String] = new java.util.HashMap[String, String]()
     options.put(Neo4jOptions.URL, "bolt://localhost")
     options.put(QueryType.QUERY.toString.toLowerCase, "MATCH (o:Object) RETURN o")
