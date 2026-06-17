651b0ed256911e47b4ab546205ad1a07fdc04dbf
emrehizal <emre.hizal@neo4j.com>
2026-06-17 16:31:02 +0200

    feat: backport indexed script support from v6 to v5 connector

--- common/src/main/scala/org/neo4j/spark/util/Validations.scala
+++ common/src/main/scala/org/neo4j/spark/util/Validations.scala
@@ -314,7 +314,7 @@ case class ValidateWrite(
       neo4jOptions.script.foreach(query =>
         ValidationUtil.isTrue(
           schemaService.isValidQuery(query),
-          s"The following query inside the `${Neo4jOptions.SCRIPT}` is not valid, please check the syntax: $query"
+          s"The following script query is not valid, please check the syntax: $query"
         )
       )
 
@@ -396,7 +396,7 @@ case class ValidateRead(neo4j: Neo4j, neo4jOptions: Neo4jOptions, jobId: String)
       ValidationUtil.isTrue(
         scriptErrors.isEmpty,
         s"""
-           |The following queries inside the `${Neo4jOptions.SCRIPT}` are not valid,
+           |The following script queries are not valid,
            |please check their syntax:
            |$scriptErrors
            |""".stripMargin
