8b22cc352c9ff0442e039ba03446f74bfd0f5d89
Erik Nord <erik.nord@neo4j.com>
2026-07-14 15:29:48 +0200

    refactor: introduce WriteIT

---
+++ src/test/scala/org/neo4j/spark/WriteIT.scala
@@ -0,0 +1,149 @@
+package org.neo4j.spark
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.SparkSession
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.params.Parameter
+import org.neo4j.caniuse.Neo4j
+import org.neo4j.driver.Driver
+import org.neo4j.driver.Value
+import org.neo4j.driver.internal.InternalIsoDuration
+import org.neo4j.spark.testsupport.InjectNeo4jContainerParameter
+import org.testcontainers.neo4j.Neo4jContainer
+
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Period
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.util.stream.Stream
+
+import scala.jdk.CollectionConverters.SeqHasAsJava
+
+@InjectNeo4jContainerParameter
+@DisplayName("writing")
+class WriteIT {
+
+  @Parameter
+  var neo4jContainer: Neo4jContainer = _
+
+  @Test
+  def throws_exception_if_no_valid_read_options_set(driver: Driver, spark: SparkSession, neo4j: Neo4j): Unit = {
+    assertThatExceptionOfType(classOf[IllegalArgumentException])
+      .isThrownBy(() => spark.read.format("neo4j").option("url", neo4jContainer.getBoltUrl).load()).withMessage(
+        "No valid option found. One of `GDS`, `LABELS`, `QUERY`, `RELATIONSHIP` is required"
+      )
+  }
+
+  @Test
+  def throws_exception_if_multiple_read_options_set(driver: Driver, spark: SparkSession, neo4j: Neo4j): Unit = {
+    assertThatExceptionOfType(classOf[IllegalArgumentException])
+      .isThrownBy(() =>
+        spark.read.format("neo4j").option(
+          "url",
+          neo4jContainer.getBoltUrl
+        ).option("labels", "Person").option("relationship", "KNOWS").load()
+      ).withMessage(
+        "You need to specify just one of these options: 'gds', 'labels', 'query', 'relationship'"
+      )
+  }
+
+  @TestFactory
+  def should_write_native_jvm_to_neo4j(driver: Driver, spark: SparkSession, neo4j: Neo4j): Stream[DynamicTest] = {
+
+    import spark.implicits._ // to import .toDF()
+    val col = "node_property"
+
+    case class JvmTestCase[FromType, ToType](
+      name: String,
+      df: DataFrame,
+      expectedValue: ToType,
+      expectedType: String,
+      accessor: Value => ToType
+    )
+
+    val cases = Seq(
+      JvmTestCase("String to String", Seq("test").toDF(col), "test", "STRING", _.asString()),
+      JvmTestCase("Long to Int", Seq(1234567890L).toDF(col), 1234567890, "INTEGER", _.asInt()),
+      JvmTestCase("Int to Int", Seq(1234567890).toDF(col), 1234567890, "INTEGER", _.asInt()),
+      JvmTestCase("Short to Int", Seq(12345.toShort).toDF(col), 12345, "INTEGER", _.asInt()),
+      JvmTestCase("Byte to Int", Seq(123.toByte).toDF(col), 123, "INTEGER", _.asInt()),
+      JvmTestCase("Double to Float", Seq(123.45).toDF(col), 123.45, "FLOAT", _.asDouble()),
+      JvmTestCase("Float to Float", Seq(123.5f).toDF(col), 123.5, "FLOAT", _.asDouble()),
+      JvmTestCase(
+        "Decimal to String",
+        Seq(BigDecimal("5.42")).toDF(col),
+        "5.420000000000000000",
+        "STRING",
+        _.asString()
+      ),
+      JvmTestCase("Boolean to Boolean", Seq(true).toDF(col), true, "BOOLEAN", _.asBoolean()),
+      JvmTestCase(
+        "Date to Date",
+        Seq(LocalDate.of(2022, 1, 1)).toDF(col),
+        LocalDate.of(2022, 1, 1),
+        "DATE",
+        _.asLocalDate()
+      ),
+      JvmTestCase(
+        "Instant to ZonedDateTime",
+        Seq(Instant.ofEpochSecond(1337)).toDF(col),
+        ZonedDateTime.ofInstant(Instant.ofEpochSecond(1337), ZoneOffset.UTC),
+        "ZONED DATETIME",
+        _.asZonedDateTime()
+      ),
+      JvmTestCase(
+        "LocalDateTime to LocalDateTime",
+        Seq(LocalDateTime.of(2022, 1, 1, 12, 0)).toDF(col),
+        LocalDateTime.of(2022, 1, 1, 12, 0),
+        "LOCAL DATETIME",
+        _.asLocalDateTime()
+      ),
+      JvmTestCase(
+        "Duration to Duration",
+        Seq(Duration.ofDays(42)).toDF(col),
+        new InternalIsoDuration(0, 42, 0, 0),
+        "DURATION",
+        _.asIsoDuration()
+      ),
+      JvmTestCase(
+        "Period to Duration",
+        Seq(Period.ofMonths(5)).toDF(col),
+        new InternalIsoDuration(5, 0, 0, 0),
+        "DURATION",
+        _.asIsoDuration()
+      )
+    )
+
+    cases.map { testCase =>
+      dynamicTest(
+        testCase.name,
+        () => {
+          val label = testCase.name.replace(" ", "_")
+          testCase.df.write.format(classOf[DataSource].getName).mode(SaveMode.Append)
+            .option("url", neo4jContainer.getBoltUrl)
+            .option("labels", label)
+            .save()
+
+          val fetchQuery: String = s"MATCH (n:`$label`) RETURN n.$col AS value, valueType(n.$col) AS type"
+          val record = driver.session().run(fetchQuery).single()
+          val actualValue = testCase.accessor(record.get("value"))
+          val actualType = record.get("type").asString().replace(" NOT NULL", "")
+
+          assertThat(actualValue).isEqualTo(testCase.expectedValue)
+          assertThat(actualType).isEqualTo(testCase.expectedType)
+        }
+      )
+    }
+      .asJava.stream()
+  }
+}
