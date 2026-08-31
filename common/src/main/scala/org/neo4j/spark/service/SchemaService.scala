ca4700e637c88b901c05332f13bdc22dfd18b5bd
Erik Nord <erik.nord@neo4j.com>
2026-08-31 11:23:47 +0200

    fix: labels containing spaces causes slowdown
    Before, we sanitized/escaped/quoted the key used to look-up the
    returned map from `apoc.meta.stats()`. However, Cypher produces these
    keys as non-escaped string values. So if we escape the predicted key,
    we will never match the produced APOC key.
    
    The symptom of this state is, instead of error and fall back to count
    query, we use Long.Max value for each executor, causing each worker
    thread to scan the whole Neo4j graph, causing slowdown.
    
    This PR fixes the error state such that we do not escape the key. And
    in the case where no matches was found, we fall back to query.

--- common/src/main/scala/org/neo4j/spark/service/SchemaService.scala
+++ common/src/main/scala/org/neo4j/spark/service/SchemaService.scala
@@ -517,19 +517,23 @@ class SchemaService(
           .getOrElse(Collections.emptyMap())
           .asInstanceOf[util.Map[String, Long]]
           .asScala
-        val minFromSource = options.relationshipMetadata.source.labels
-          .map(_.quote())
+
+        val minFromSource = options.relationshipMetadata.source.labels // no sanitization here - labels used as map keys
           .map(label =>
-            map.get(s"(:$label)-[:${options.relationshipMetadata.relationshipType}]->()").getOrElse(Long.MaxValue)
+            map.get(s"(:$label)-[:${options.relationshipMetadata.relationshipType}]->()")
           )
-          .min
-        val minFromTarget = options.relationshipMetadata.target.labels
-          .map(_.quote())
+
+        val minFromTarget = options.relationshipMetadata.target.labels // no sanitization here - labels used as map keys
           .map(label =>
-            map.get(s"()-[:${options.relationshipMetadata.relationshipType}]->(:$label)").getOrElse(Long.MaxValue)
+            map.get(s"()-[:${options.relationshipMetadata.relationshipType}]->(:$label)")
           )
-          .min
-        Math.min(minFromSource, minFromTarget)
+
+        if (minFromSource.contains(None) || minFromTarget.contains(None)) {
+          logResolutionChange("Count store lookup incomplete. Switching to query count resolution", null)
+          countForRelationshipWithQuery(filters)
+        } else {
+          Math.min(minFromSource.flatten.min, minFromTarget.flatten.min)
+        }
       } else {
         countForRelationshipWithQuery(filters)
       }
@@ -553,6 +557,7 @@ class SchemaService(
       Seq(skipLimit)
     } else {
       val count: Long = this.count()
+
       if (count <= 0) {
         Seq(PartitionPagination.EMPTY)
       } else {
@@ -1009,7 +1014,8 @@ class SchemaService(
 
   private def logResolutionChange(message: String, e: ClientException): Unit = {
     log.warn(message)
-    if (!e.code().equals("Neo.ClientError.Procedure.ProcedureNotFound")) {
+
+    if (e != null && !e.code().equals("Neo.ClientError.Procedure.ProcedureNotFound")) {
       log.warn(s"For the following exception", e)
     }
   }
